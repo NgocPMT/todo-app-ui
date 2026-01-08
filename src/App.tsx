e14bf89c7d090e5f068ab83729736dd0514f11fb
Nathan Pham <np1552005@gmail.com>
2026-01-08 02:02:09 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="bg-yellow-50">
+      <header className="py-2 px-3">
+        <nav>
+          <a className="text-lg font-semibold">Todo App</a>
+        </nav>
+      </header>
+      <main></main>
+    </div>
+  );
+}
+
+export default App;
