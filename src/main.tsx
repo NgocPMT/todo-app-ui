6fe8e32903719dd6245cc55e7370d94f23f9ade3
Nathan Pham <np1552005@gmail.com>
2026-01-09 04:11:01 +0000

    chore: rename DayjsWrapper component so it match the naming convention

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import DayjsWrapper from "./components/DayjsWrapper.tsx";
+import DayjsWrapper from "./components/dayjs-wrapper.tsx";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
